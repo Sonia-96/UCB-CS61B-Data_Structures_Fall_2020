bef3b51e4723e4847bea63cc0bae4a84c2571eb7
Sonia-96 <672365639@qq.com>
2021-03-19 11:14:07 +0800

    change size from protected to be private

--- proj1b/ArrayDeque.java
+++ proj1b/ArrayDeque.java
@@ -2,7 +2,7 @@ public class ArrayDeque<T> implements Deque<T> {
     private T[] items;
     private int nextFirst;
     private int nextLast;
-    protected int size;
+    private int size;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
