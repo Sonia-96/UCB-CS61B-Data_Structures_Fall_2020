7a3b4118b4b71ea8d666a2cfb91fc2a58061c7fe
Sonia-96 <672365639@qq.com>
2021-04-02 11:20:37 +0800

    finished hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -11,43 +11,51 @@ public class Percolation {
     int openSites;
 
     public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be greater than 0!");
+        }
         this.N = N;
         open = new int[N * N + 2];
         openSites = 0;
         uf = new WeightedQuickUnionUF(N * N + 2);
-        for (int i = 1; i <= N; i++) {
-            uf.union(0, i);
-        }
-        for (int i = xyTo1D(N-1, 0); i <= N * N; i++) {
-            uf.union(N * N + 1, i);
-        }
     }
 
     public void open(int row, int col) {
+        validate(row, col);
         int p = xyTo1D(row, col);
         if (open[p] == 0) {
             open[p] = 1;
             openSites += 1;
+            if (row == 0) {
+                uf.union(0, p);
+            } // 如果在第一排，则和顶部联通
+
             if (p - N > 0 && open[p - N] == 1) {
                 uf.union(p, p - N);
-            }
+            } // 上
             if (p + N <= N * N && open[p + N] == 1) {
                 uf.union(p, p + N);
-            }
+            } // 下
             if (p - 1 > 0 && open[p - 1] == 1) {
                 uf.union(p, p - 1);
-            }
+            } // 左
             if (p + 1 <= N * N && open[p + 1] == 1) {
                 uf.union(p, p + 1);
-            }
+            } // 右
+
+            if (row == N - 1 && uf.connected(0, p)) {
+                uf.union(N * N + 1, p);
+            } // 如果在最后一排且和顶部联通，则和底部联通
         }
     }
 
     public boolean isOpen(int row, int col) {
+        validate(row, col);
         return open[xyTo1D(row, col)] == 1;
     }
 
     public boolean isFull(int row, int col) {
+        validate(row, col);
         return uf.connected(0, xyTo1D(row, col));
     }
 
@@ -59,8 +67,17 @@ public class Percolation {
         return uf.connected(0, N * N + 1);
     }
 
-    private int xyTo1D(int r, int c) {
-        return r * N + c + 1;
+    private int xyTo1D(int row, int col) {
+        return row * N + col + 1;
+    }
+
+    private void validate(int row, int col) {
+        if (row < 0 || row > N - 1) {
+            throw new IndexOutOfBoundsException("row " + row + " is not between 0 and " + (N - 1));
+        }
+        if (col < 0 || col > N - 1) {
+            throw new IndexOutOfBoundsException("col " + col + " is not between 0 and " + (N - 1));
+        }
     }
 
     @Test
@@ -79,6 +96,7 @@ public class Percolation {
         assertFalse(P.percolates());
         P.open(4, 4);
         assertTrue(P.percolates());
+        P.open(4, 2);
+        assertFalse(P.isFull(4, 2));
     }
-
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,41 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import static java.lang.Math.sqrt;
+
 public class PercolationStats {
+    double[] openFraction;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        openFraction = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation P = pf.make(N);
+            while (!P.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                P.open(row, col);
+            }
+            double temp = P.numberOfOpenSites() * 1.0 /  (N * N);
+            openFraction[i] = temp;
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(openFraction);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(openFraction);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / sqrt(openFraction.length);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / sqrt(openFraction.length);
+    }
 
 }
